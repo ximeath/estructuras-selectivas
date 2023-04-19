6682e4541a52bc7a3ab8765d9050a915306d75b5
ximeath <ximenaatehortuahernandez@iesantaelena.edu.co>
2023-04-19 16:24:04 -0500

    modificacion ejercicios

--- script/ejercicio3.js
+++ script/ejercicio3.js
@@ -1,3 +1,22 @@
-let nombre,salariob,deducciones,bonificaciont1,bonificaciont2
+let nombre,salario,deducciones,bonificaciones,nombre1,nombre2
 
 //asignar imformacion 
+
+nombre = prompt("ingrese nombre del trabajador 1")
+nombre2 = prompt("ingrese nombre del trabajador 2")
+salario = Number(prompt("ingrese el salario del empleado 1"))
+salario2 = Number(prompt("ingrese el salario del empleado 2"))
+deducciones = Number(prompt("ingrese deducciones del trabajador 1"))
+deducciones2 = Number(prompt("ingrese deducciones del trabajador 2"))
+bonificaciones = Number(prompt("ingrese las bonificaciones del trabajador 1"))
+bonificaciones2 = Number(prompt("ingrese las bonificaciones del trabajador 2"))
+
+
+//proceso 
+
+
+if (salario > salario2) {
+    alert("el salario mayor es: " + nombre)
+} else {
+    alert("el salario mayor es: " + nombre2)
+}
